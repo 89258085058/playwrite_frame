94df1d6f6051fa48afbc558dee5490057b513da5
89258085058 <gorleov2895@yandex.ru>
2023-11-15 15:58:21 +0300

    Start playwrite 3

--- constants/constants_main.py
+++ constants/constants_main.py
@@ -1,7 +1,7 @@
 class MainConstants:
 
-    URL: str = "http://84.201.143.9"
+    URL: str = "******************"
 
-    login: str = "qa-gorelov@mail.ru"
-    password: str = "Bolid-57"
+    login: str = "*******"
+    password: str = "******"
 
